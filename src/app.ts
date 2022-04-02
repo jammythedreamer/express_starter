f2f306deb40e53f9b63f137dd361ada72c0e12db
jammythedreamer <jammythedreamer@gmail.com>
2022-04-03 03:59:48 +0900

    use docker-compose, add mysql, redis

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import * as express from "express";
+import * as express from 'express';
 
+import * as logic_test from './LOGIC/logic_test';
+import * as redis from './DATABASE/redis';
 class App {
   public application: express.Application;
 
@@ -13,12 +15,30 @@ const app = new App().application;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   res.send("hello, world!");
 });
 
-app.get("/health", (req, res) => {
+app.get('/health', (req, res) => {
   res.send("OK");
 });
 
-app.listen(4000, () => console.log("start"));
+app.get('/admin', (req, res) => {
+  console.log("/admin");
+  res.send("TODO");
+});
+
+app.post('/setData', async (req, res) => {
+  const key = req.body.key;
+  const value = req.body.value;
+  const result = await redis.setData(key, value);
+  res.send(result);
+})
+
+app.get('/getData', async (req, res) => {
+  const key = req.query.key as string;
+  const result = await redis.getData(key);
+  res.send(result);
+})
+
+app.listen(4000, () => console.log('start'));
